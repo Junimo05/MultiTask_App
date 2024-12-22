3fe6bcbf177a4d01f2663e224545c532ea689c7d
Nguyen Anh Tuan <ungvang67@gmail.com>
2024-12-23 04:17:00 +0700

    Revert "Init Commit"
    This reverts commit 00f7b56dda35d1da224034fc66f3a13a14962aa6.

--- multitask_app/android/app/src/main/kotlin/com/example/verygoodcore/multitask/app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.verygoodcore.multitask_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- multitask_app/lib/main_staging.dart
+++
@@ -1,6 +0,0 @@
-import 'package:multitask_app/app/app.dart';
-import 'package:multitask_app/bootstrap.dart';
-
-void main() {
-  bootstrap(() => const App());
-}
