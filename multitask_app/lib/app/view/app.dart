70fcce43706bb3d335166667e8bb5a7058b546a6
Nguyen Anh Tuan <ungvang67@gmail.com>
2024-12-23 18:01:42 +0700

    #1

--- multitask_app/lib/app/view/app.dart
+++ multitask_app/lib/app/view/app.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:multitask_app/counter/counter.dart';
 import 'package:multitask_app/l10n/l10n.dart';
+
+final _router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const CounterPage(),
+    ),
+    GoRoute(
+      path: '/counter',
+      builder: (context, state) => const CounterPage(),
+    ),
+    // GoRoute(
+    //   path: '/weather',
+    //   builder: (context, state) => const WeatherPage(),
+    // )
+  ],
+);
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       theme: ThemeData(
         appBarTheme: AppBarTheme(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -16,7 +35,7 @@ class App extends StatelessWidget {
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+      routerConfig: _router,
     );
   }
 }
